dbca27bf28518a4abce7be4b88cf6cea6e1ac579
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-03-12 12:42:55 -0500

    Cambiado el footer y el landing

---
+++ src/Components/Banner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./bannerStyles.css";
+
+const Landing = () => {
+  return (
+    <div className="bannerClassName">
+      <div class="boxed"> <h1>Utiliza JS MUSIC, Registrate ahora mismo</h1></div>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ src/Components/Landing/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Banner from "../Banner/index";
+
+const Landing = () => {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col"><Banner/></div>
+        <div class="col">2 of 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
